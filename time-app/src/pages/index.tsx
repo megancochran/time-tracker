ac04ff1d465e585dae76eec09dae872b6dbebd29
megancochran <mecochran13@gmail.com>
2023-04-05 15:51:21 -0400

    tailwind implementation

--- time-app/src/pages/index.tsx
+++ time-app/src/pages/index.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex h-screen min-w-full flex-col flex-wrap content-center justify-center gap-y-8">
+      <h1 className="text-grey-800">00:00:00</h1>
+      <button className="self-center">Start Session</button>
+    </div>
+  );
 }
--- time-app/tailwind.config.js
+++ time-app/tailwind.config.js
@@ -9,7 +9,52 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      // B&W
+      black: "#000000",
+      white: "#ffffff",
+      // Greyscale
+      "grey-900": "#212529",
+      "grey-800": "#343A40",
+      "grey-700": "#495057",
+      "grey-600": "#6C757D",
+      "grey-500": "#ADB5BD",
+      "grey-400": "#CED4DA",
+      "grey-300": "#DEE2E6",
+      "grey-200": "#E9ECEF",
+      "grey-100": "#F8F9FA",
+      // Primary: Blue
+      "blue-500": "#2B5985",
+      "blue-400": "#4690D6",
+      "blue-300": "#6FB9FF",
+      "blue-200": "#A6D4FF",
+      "blue-100": "#CFE8FF",
+      // Secondary: Peach
+      "peach-500": "#FFBB89",
+      "peach-400": "#FFC69C",
+      "peach-300": "#FFD1AF",
+      "peach-200": "#FFDCC2",
+      "peach-100": "#FFE7D5",
+    },
+    fontFamily: {
+      sans: ["Satoshi", "sans-serif"],
+    },
+
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
   },
   plugins: [],
 };
