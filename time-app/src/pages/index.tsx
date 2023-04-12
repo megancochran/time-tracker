8f8fe37f800a00211e7794208864f48f6722beb8
megancochran <mecochran13@gmail.com>
2023-04-12 11:33:15 -0400

    daisy UI

--- time-app/src/pages/index.tsx
+++ time-app/src/pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex h-screen min-w-full flex-col flex-wrap content-center justify-center gap-y-8">
       <h1 className="text-grey-800">00:00:00</h1>
-      <button className="self-center">Start Session</button>
+      <button className="btn-primary btn">Start Session</button>
     </div>
   );
 }
